41165a750332b1b62b04d5c9b743342601fca330
Edouard Paris <m@edouard.paris>
2020-04-21 10:23:47 +0200

    doc: add example

--- app-btc/src/lib.rs
+++ app-btc/src/lib.rs
@@ -1,4 +1,27 @@
 //! https://github.com/edouardparis/ledger-hw
+//!
+//! Example:
+//!
+//! ```
+//! use std::str::FromStr;
+//! use bitcoin::util::bip32::DerivationPath;
+//! use ledger_hw_transport_hid::HidTransport;
+//!
+//! use ledger_hw_app_btc::{AddressFormat, get_wallet_public_key};
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let transport = HidTransport::new().unwrap();
+//!     let path = DerivationPath::from_str("m/44'/0'/0'/0/0").unwrap();
+//!     let format = AddressFormat::Legacy;
+//!     let (pk, address, chaincode) = get_wallet_public_key(&transport, &path, true, format)
+//!         .await
+//!         .unwrap();
+//!     println!("pk: {}", pk);
+//!     println!("address: {}", address);
+//!     println!("chaincode: {}", chaincode);
+//! }
+//! ```
 
 pub mod constant;
 pub mod error;
